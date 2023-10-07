036e5046afb4a63d9d726968477c7f428f1eaa59
PuspitaSingha07 <puspitasingha128c.com>
2023-10-07 06:53:13 +0600

    Required Folders are Created

--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,30 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import Root from './Components/Root/Root';
+import Home from './Components/Home/Home';
+import Login from './Components/Login/Login';
+import Registration from './Components/Registration/Registration';
+
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Root></Root> ,
+    children:[
+      {
+        path:"/",
+        element:<Home></Home>
+      },
+      {
+        path:"/login",
+        element:<Login></Login>
+      },
+      {
+        path:"/registration",
+        element: <Registration></Registration>
+      }
+    ]
   },
 ]);
 
